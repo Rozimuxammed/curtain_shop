48feb27faa829a4b02ffcc341f594eb8dbdf7083
Rozimuhammad Rozimurodov <rozimuhammadrozimurodov406@gmail.com>
2025-09-15 20:29:45 +0500

    deleted fragment

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Fragment } from "react";
 
 export default function MainLayout() {
   return (
