2b733b9cf8b5d6e491f02c201c115d366d470668
Rozimuhammad Rozimurodov <rozimuhammadrozimurodov406@gmail.com>
2025-09-16 19:46:04 +0500

    changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,9 @@
 import ProductCard from "../components/ProductCard";
-// const products = [
-//   {
-//     id: 1,
-//     name: "Elegant Sheer Fabric",
-//     price: 15,
-//     image:
-//       "https://alnassaj.com/wp-content/uploads/2024/10/WayfairBasics%C2%AERothwellModernSheerRodPocketCurtainPanel.webp",
-//   },
-//   {
-//     id: 2,
-//     name: "Blackout Curtain Material",
-//     price: 20,
-//     image:
-//       "https://lirp.cdn-website.com/24c2a0ba/dms3rep/multi/opt/triple-weave-fabric-640w.jpg",
-//   },
-//   {
-//     id: 3,
-//     name: "Elegant Sheer Fabric",
-//     price: 15,
-//     image:
-//       "https://alnassaj.com/wp-content/uploads/2024/10/WayfairBasics%C2%AERothwellModernSheerRodPocketCurtainPanel.webp",
-//   },
-//   {
-//     id: 4,
-//     name: "Blackout Curtain Material",
-//     price: 20,
-//     image:
-//       "https://lirp.cdn-website.com/24c2a0ba/dms3rep/multi/opt/triple-weave-fabric-640w.jpg",
-//   },
-//   {
-//     id: 5,
-//     name: "Elegant Sheer Fabric",
-//     price: 15,
-//     image:
-//       "https://alnassaj.com/wp-content/uploads/2024/10/WayfairBasics%C2%AERothwellModernSheerRodPocketCurtainPanel.webp",
-//   },
-//   {
-//     id: 6,
-//     name: "Blackout Curtain Material",
-//     price: 20,
-//     image:
-//       "https://lirp.cdn-website.com/24c2a0ba/dms3rep/multi/opt/triple-weave-fabric-640w.jpg",
-//   },
-//   {
-//     id: 7,
-//     name: "Elegant Sheer Fabric",
-//     price: 15,
-//     image:
-//       "https://alnassaj.com/wp-content/uploads/2024/10/WayfairBasics%C2%AERothwellModernSheerRodPocketCurtainPanel.webp",
-//   },
-//   {
-//     id: 8,
-//     name: "Blackout Curtain Material",
-//     price: 20,
-//     image:
-//       "https://lirp.cdn-website.com/24c2a0ba/dms3rep/multi/opt/triple-weave-fabric-640w.jpg",
-//   },
-// ];
+
 export default function Home() {
-  return <ProductCard />;
+  return (
+    <h1>Home pageda landing page boladi landing pageni orniga home pageni qvorarmz 
+      
+    </h1>
+  );
 }
